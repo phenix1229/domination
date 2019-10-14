806f43e906ad1e97fcde430faa6c7340f8710e4c
phenix1229 <paul.garay@codeimmersives.com>
2019-10-14 19:42:33 -0400

    completed all functions and tasks

--- main.js
+++ main.js
@@ -27,5 +27,41 @@ removeLi();
 
 function appendToUl(element){
     const ul = document.querySelector('#arguments');
-    ul.appendChild
+    const newChild = document.querySelector(element);
+    ul.appendChild(newChild)
 }
+
+appendToUl('#image-2');
+
+function setImgHeight(img){
+    document.querySelector(img).style.height = '30px';
+}
+
+setImgHeight('#image-1');
+
+function setInvisible(element){
+    document.querySelector(element).setAttribute ('class', 'invisible');
+}
+
+setInvisible('ol');
+
+function createHeader(headerNum, innerTxt){
+    newHeader = document.createElement('h' + headerNum);
+    newHeader.setAttribute('class', 'headR');
+    newHeader.innerText = innerTxt;
+    return document.querySelector('body').appendChild(newHeader);
+}
+
+const newH = createHeader('1', 'test header')
+
+appendToUl('.headR');
+
+function newLi(liText){
+const newItem = document.createElement('li');
+    newItem.innerText = liText;
+    newItem.setAttribute('class', 'newLItem')
+    return document.querySelector('body').appendChild(newItem);
+}
+
+const testLi = newLi('This is a test of newLi!');
+appendToUl('.newLItem');
